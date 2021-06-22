d0976eceb365b805b304b22fa1978af21c2a89e7
Yukwok <billynyk@gmail.com>
2021-06-22 18:26:51 +0800

    get , post to google sheet

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from datetime import datetime
 import requests
-
 
 GENDER = "M"
 WEIGHT_KG = "90"
 HEIGHT_CM = "180"
-AGE = "45"
+AGE = "47"
 
 APP_ID = "a62bb22a"
 API_KEY = "8a0cfb1927e2b85bdd09d8f0ef76bf61"
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheety_endpoint = "https://api.sheety.co/e02e90cc81d3e6b66814fe14bef27a3e/trainingWorkouts/workouts"
+
 
 exercise_text = input("Tell me which exercises you did: ")
 
@@ -26,6 +28,40 @@ parameters = {
     "age": AGE
 }
 
-response = requests.post(exercise_endpoint, json=parameters, headers=headers)
-result = response.json()
-print(result)12
+response = requests.post(url=exercise_endpoint,
+                         headers=headers, json=parameters)
+exercise_0 = response.json()["exercises"][0]
+
+# print(exercise_0["user_input"])
+# print(exercise_0["duration_min"])
+# print(exercise_0["nf_calories"])
+
+exercise = exercise_0["user_input"]
+duration = exercise_0["duration_min"]
+calories = exercise_0["nf_calories"]
+
+
+today_date = datetime.today().strftime("%d/%m/%Y")
+today_time = datetime.today().strftime("%H:%M:%S")
+
+
+print(today_time)
+
+
+sheety_parameters = {
+    "workout":
+    {"exercise": exercise,
+     "duration": duration,
+     "calories": calories,
+     "date": today_date,
+     "time": today_time, }
+}
+
+
+response = requests.post(url=sheety_endpoint, json=sheety_parameters)
+workouts = response.json()
+print(workouts)
+
+response = requests.get(url=sheety_endpoint)
+workouts = response.json()["workouts"]
+print(workouts)
